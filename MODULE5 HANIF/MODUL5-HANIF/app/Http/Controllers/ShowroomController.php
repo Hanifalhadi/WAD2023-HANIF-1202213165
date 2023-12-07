bd007dac562cb047771a57d5ea3997f68c5a7674
Hanifalhadi <hanifalhadi@student.telkomuniversity.ac.id>
2023-12-07 08:50:08 +0700

    Module5

---
+++ MODULE5 HANIF/MODUL5-HANIF/app/Http/Controllers/ShowroomController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Showroom;
+use Illuminate\Http\Request;
+
+class ShowroomController extends Controller
+{
+    //
+    public function create(){
+        return view('showroom.create');
+    }
+    public function store(Request $request){
+    $data = $request->all();
+    
+    Showroom::create([
+        'nama_mobil' => $data['nama_mobil'],
+        'brand_mobil' => $data['brand_mobil'],
+        'warna_mobil' => $data['warna_mobil'],
+        'tipe_mobil' => $data['tipe_mobil'],
+        'harga_mobil' => $data['harga_mobil'],
+    ]);
+    return redirect(route('showroom.index'));
+    }
+    public function index(){
+        $showroom = Showroom::all();
+        return view ('showroom.index', compact('showroom'));
+    }
+}
